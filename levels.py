f40cf43810f6671c7188aa364473e0b5edb30a54
Weijing Wang <weijingrocks@gmail.com>
2023-03-31 22:06:03 -0700

    hair machine

--- levels.py
+++ levels.py
@@ -27,8 +27,8 @@ level1 = [
     'BBBBBBBBBBBBBBBBBBBB'
 ]
 
-description2 = ('i have droven may car,',
-                'they guards will not notice me, i said, if i conceal my person')
+description2 = ('these foolish guards will not notice me if i concel my person in the shadows',
+                'in short, I must hide in the shadows, or under a goat')
 level2 = [
     'BBBBBBBBBBBBBBBBBBBB',
     'B~~~.........RRRRRR_',
@@ -49,7 +49,8 @@ level2 = [
 #     '.BBBBBBBBB...BBB.....'
 # ]
 
-description3 = ('by holy mary! brother there are many goats.','')
+description3 = ('by holy mary! brother there are many goats.',
+                'each will all attain their freedom, if I can help it')
 level3 = [
     'BBBBBBBBBBBBBBBBDBBB',
     'BRRRRRRRRRRRRRRRRRRB',
@@ -61,8 +62,8 @@ level3 = [
     'BBBBBBBBBBBBBBBBBBBB'
 ]
 
-description4 = ('what is this, i tell you i despise you',
-                'never seek to touch goats against their will')
+description4 = ('never seek to touch goats against their will',
+                'the goats salvation, i said, the remedy depends entirely on me.')
 # description4 = ('the guards are too stupid i will sneak past the fools',
 #                 'now i know their secret they use the humans to make beautifl goats.')
 
@@ -77,14 +78,15 @@ level4 = [
 ]
 
 
-description5 = ('i will take this secret to my grave and I will share it with all my friends.','')
+description5 = ('i will take this secret to my grave and I will share it with all my friends.',
+                'godwilling, I will save these goats from these churls')
 
 level5 = [ 
     'BBBBBBBBBBBBBBBBBBBB',
     'B.EE....EE.........B',
-    'BRRR..RRRE......B',
-    'BRRRSIRGR.E........B',
-    'BGRGS.RRRE.........B',
+    'BRRRSSSRRE......B',
+    'BRRRSISGR.E........B',
+    'BGRGS.SRRE.........B',
     'BGRR..RRRE.........B',
     'BRRRRRRGR..........B',
     'BBDBBBBBBBBBBBBBBBBB'
@@ -93,7 +95,7 @@ level5 = [
 
 # store the levels and descriptions in arrays
 levels = [level0, level1, level2, level3, level4, level5]
-level_descrip = [description0, description4, description2, description3, description4, description5]
+level_descrip = [description0, description1, description2, description3, description4, description5]
 
 
 
@@ -102,7 +104,7 @@ intro_text = ['by holy mary, these are the best goats i have ever seen',
         'I said: sir price please',
         'he says: brother no sale',
         'I am brought by deception to the greatest treachery in the world',
-        'in my name, then I will come to your farm'
+        'in gods name, I will come to your farm'
         ]
 
 #INTRO IMAGES LOADED IN GAME CLASS RIGHT NOW
